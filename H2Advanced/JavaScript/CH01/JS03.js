744125af4e35eb91c64a7471c4460cb76de3b6d4
ilikui <2563043733@qq.com>
2022-05-29 11:08:41 +0800

    update

--- H2Advanced/JavaScript/CH01/JS03.js
+++ H2Advanced/JavaScript/CH01/JS03.js
@@ -1,5 +1,9 @@
 document.getElementById('btn').onclick=function(){
-    alert(1);
+    //alert(1);
+    document.getElementById('box').style.background='red';
+    document.getElementById('box').style.height='100px';
+    document.getElementById('box').style.width='100px';
+
 }
 
 //单行注释
